52a85f07a4fa064cb814d5aa1feb0246f6374e2a
cs222wa <cs222wa@studen.lnu.se>
2014-11-12 14:10:35 +0100

    Cleared test 1.1 - Function is returning an object.

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -1,39 +1,43 @@
 "use strict";
 
-var makePerson = function(persArr){
+var makePerson = function(persArr){ //tar emot en array som argument och gör om det till ett objekt. (persArr)
 
-/*Det objekt som returneras ska innehålla information om
-den högsta åldern, lägsta åldern, medelåldern
-samt en sträng med samtliga namn sorterade i bokstavsordning.*/
+{ var result = {};
 
 
-/*Objekten i arrayen som skickas som argument ska innehålla två egenskaper:
-
-name - sträng innehållande för- och efternamn
-age - nummer innehållande ålder
-Exempel på array som skickas som argument till funktionen:
-js var data = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36}, {name: "Mats Loock", age: 46}];
-*/
 
 
-/*Objektet som returneras ska innehålla fyra egenskaper:
-minAge - nummer innehållande den ålder i de inskickade objekten som är lägst.
-maxAge -nummer innehållande den ålder i de inskickade objekten som är högst.
-avarageAge - nummer innehållande medelåldern av de inskickade objektens ålder.
-names - sträng innehållande samtliga personers namn separerade med ", " (komma och efterföljande mellanslag). Namnen sorteras i bokstavsordning.
-Testa genom att skriva ut resultatet till konsolen:
 
-```js var makePerson = function(persArr) { var result = {};
 
-// Lös uppgiften
-return result; } var data = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36}, {name: "Mats Loock", age: 46}];
+	var names = persArr.map(function (name){
+		return name.names;
+	});
+
+var names = persArr.Name;
+var ages = persArr.Age;
+
+var minAge;
+var maxAge;
+var averageAge;
+
+
+
+
+
+
+
+
+
+return result; }
+
+
+var data = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36}, {name: "Mats Loock", age: 46}];
+
 var result = makePerson(data);
-console.log(result); ```
-*/
 
-
-//Skapa en array som tar namn, ålder.
-//Objekten i arrayen ska sorteras och skrivas ut:
-//Högsta, lägsta och medelålder + ens träng med namnen sorterade i bokstavsordning.
-
+console.log(result);
 }
+
+/*Informationen i persArr ska sorteras och skrivas ut:
+Högsta, lägsta och medelålder + en sträng med
+namnen sorterade i bokstavsordning och separerade med ", " (komma och efterföljande mellanslag). */
