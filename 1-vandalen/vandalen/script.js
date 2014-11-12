2b40dcaa28b319027be30062198afe1e8c588366
cs222wa <cs222wa@studen.lnu.se>
2014-11-12 16:07:43 +0100

    Created new array for names using .map. sorted the array and named the sorted version 'names'.

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -1,20 +1,23 @@
 "use strict";
 
-var makePerson = function(persArr){ //tar emot en array som argument och gör om det till ett objekt. (persArr)
+/*Informationen i persArr ska sorteras och skrivas ut:
+Högsta, lägsta och medelålder + en sträng med
+namnen sorterade i bokstavsordning och separerade med ", " (komma och efterföljande mellanslag). */
 
-{ var result = {};
+var makePerson = function(persArr){			//Tar emot en array (data) som argument och gör om det till ett objekt (persArr).
+
+	var namesArr = persArr.map(function(name){  //.map hämtar ut värdet från varje person i arrayen och skapar en ny array (föreläsning 3)
+    return name.names;											//returnerar value (t.ex. Johan Leitet) ifrån egenskapen name i arrayen persArr.
+  });
+
+	var names = namesArr.sort();		//sortera den nya arrayen innehållande namn.
+
+	// var ages = persArr.map(function(ages){		//se ovan, men arrayen som skapas innehåller värdena för ages.
+	// 	return age.ages;
+	// });
+	//
 
 
-
-
-
-
-	var names = persArr.map(function (name){
-		return name.names;
-	});
-
-var names = persArr.Name;
-var ages = persArr.Age;
 
 var minAge;
 var maxAge;
@@ -22,22 +25,32 @@ var averageAge;
 
 
 
+{var result = {names: names};
+return result;}
+};
+
+//
+// var data = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36}, {name: "Mats Loock", age: 46}];  //Arrayen som innehåller informationen.
+//
+// var result = makePerson(data);  //Arrayen som ska behandlas skickas till makePerson.
+//
+// console.log(result); //resultatet av makePerson skrivs ut.
 
 
+/*
+jag använde aldrig det som Johan skrev i labben.
+Ta bort det och kör en for loop med persArr.lenght.
+Plocka ut text persArr[i].name och spara dem i en ny array som du kan sortera i bokstavsordning.
+Logga ut det så kommer du börja förstå.
+Så var det för mig iallafall.
 
+Gjorde om min nya array till en sträng med alla namnen och
+satte det som result.names = strängen…
+Sist returnerar jag result.
 
-
-
-return result; }
-
-
-var data = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36}, {name: "Mats Loock", age: 46}];
-
-var result = makePerson(data);
-
-console.log(result);
-}
-
-/*Informationen i persArr ska sorteras och skrivas ut:
-Högsta, lägsta och medelålder + en sträng med
-namnen sorterade i bokstavsordning och separerade med ", " (komma och efterföljande mellanslag). */
+Du ska bara ha en return i koden (sist).
+Result är ett nytt objekt där du samlar alla dina nya egenskaper
+som textsträngen med alla namn eller när du räknat ut average.
+Du kan inte använda dig av Johans array rakt av utan måste
+bearbeta lite innan så det bli rätt
+*/
