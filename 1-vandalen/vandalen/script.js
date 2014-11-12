91f2dbb6147338e039fffb18d24d86b057c9d14b
cs222wa <cs222wa@studen.lnu.se>
2014-11-12 11:23:00 +0100

    Copied isntructions to js document.

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -2,8 +2,38 @@
 
 var makePerson = function(persArr){
 
+/*Det objekt som returneras ska innehålla information om
+den högsta åldern, lägsta åldern, medelåldern
+samt en sträng med samtliga namn sorterade i bokstavsordning.*/
 
-	// Din kod här...
+
+/*Objekten i arrayen som skickas som argument ska innehålla två egenskaper:
+
+name - sträng innehållande för- och efternamn
+age - nummer innehållande ålder
+Exempel på array som skickas som argument till funktionen:
+js var data = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36}, {name: "Mats Loock", age: 46}];
+*/
+
+
+/*Objektet som returneras ska innehålla fyra egenskaper:
+minAge - nummer innehållande den ålder i de inskickade objekten som är lägst.
+maxAge -nummer innehållande den ålder i de inskickade objekten som är högst.
+avarageAge - nummer innehållande medelåldern av de inskickade objektens ålder.
+names - sträng innehållande samtliga personers namn separerade med ", " (komma och efterföljande mellanslag). Namnen sorteras i bokstavsordning.
+Testa genom att skriva ut resultatet till konsolen:
+
+```js var makePerson = function(persArr) { var result = {};
+
+// Lös uppgiften
+return result; } var data = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36}, {name: "Mats Loock", age: 46}];
+var result = makePerson(data);
+console.log(result); ```
+*/
+
+
+//Skapa en array som tar namn, ålder.
+//Objekten i arrayen ska sorteras och skrivas ut:
+//Högsta, lägsta och medelålder + ens träng med namnen sorterade i bokstavsordning.
 
 }
-
