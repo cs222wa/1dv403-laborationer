a910a1adc71181a74f0535874a81a6151c064955
Carolina Skov Pedersen <cs222wa@student.lnu.se>
2014-11-13 09:34:29 +0000

    Got minAge and maxAge to pass test. Moving on to averageAge.

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -4,30 +4,67 @@
 Högsta, lägsta och medelålder + en sträng med
 namnen sorterade i bokstavsordning och separerade med ", " (komma och efterföljande mellanslag). */
 
-var makePerson = function(persArr){			//Tar emot en array (data) som argument och gör om det till ett objekt (persArr).
+var makePerson = function(persArr){					//Tar emot en array (data) som argument och gör om det till ett objekt (persArr).
 
+ 
+
+
+	//FUNGERAR
 	var namesArr = [];
 	var agesArr = [];
 	
-	function getProperties(elem, index, arr){
+	function getProperties(elem, index, arr){		//Letar upp indexvärdena för elementen ålder/namn en array och sparar dem till varsin ny array
 		namesArr[index] = elem.name;
 		agesArr[index] = elem.age;
 	}
 	
-	persArr.forEach(getProperties);
+	persArr.forEach(getProperties);					//Skickar arrayen persArray till funktionen getProperties
 	
-	namesArr.sort(function(a, b){
-		return a.localeCompare(b);
+	namesArr.sort(function(a, b){					//sorterar elementen i arrayen för namn (inklusive Å, Ä och Ö)
+		return a.localeCompare(b);		
 	});
 	
-	var namesStr = namesArr.join(", ");
-
-	agesArr.sort()	
-		
-	var minAge = agesArr; 	
+	var namesStr = namesArr.join(", ");				//Sätter ihop alla elementen i arrayen för namn till en sträng, åtskildja av ett "," och ett mellanslag.
 	
-	//
+	
+	
+	
+	
+	//Nytt problem -  Åldrarna skrivs ut som undefined & siffror upprepade gånger. - Står fortfarande i en forloop??
+	
+	var agesSorted = agesArr.sort(function(a, b){return a-b});		//sorterade åldrarna i stigande nummerordning
+	
+	 
+	 var minAge = agesSorted[0];		//= ger rätt på test 1.3 - men returnerar hela arrayen.
+	 
+	var maxAge = agesSorted[agesSorted.length - 1];
+	 
+	 
+	 
+	 
+	 
+	//VARFÖR FUNGERAR INTE DETTA?
+	 
+	//var names = persArr.map(function(people){  //.map hämtar ut värdet från varje person i arrayen och skapar en ny array (föreläsning 3)
+ 	//    return people.name;												//genom att skapa en funktion.
+ 	// });
+	
+	// function people(persArr){						//en funktion som heter names.
+	//     return names.sort(function (a, b){			//sorterar namnen i bokstavsordning
+	//         return a.localeCompare(b);					//sorterar efter Å Ä och Ö.
+	//     }).join(", ");}													//skriver ihop alla namn till en och samma sträng.
+	 
+	//	var agesArr = persArr.map(function(age){		//se ovan, men arrayen som skapas innehåller värdena för ages.
+ 	// 	return age.ages;
+ 	// });
+
 	// var agesSorted = agesArr.sort(function(a, b){return a-b});		//sorterade åldrarna i stigande nummerordning
+
+	 
+	// var minAge = agesSorted.min(function(agesSorted){				//Letade upp Minvärdet i den sorterade Arrayen för åldrar.
+	// 	return Math.min.apply(Math, agesSorted)
+	// });
+	
 	//
 	// var maxAge = agesSorted.max(function(agesSorted){					//Letade upp Maxvärdet i den sorterade Arrayen för åldrar.
 	//     return Math.max.apply(Math, agesSorted);
@@ -47,21 +84,15 @@ var makePerson = function(persArr){			//Tar emot en array (data) som argument oc
 
 
 
-	 // var names = persArr.map(function(name){  //.map hämtar ut värdet från varje person i arrayen och skapar en ny array (föreläsning 3)
- 	 //    return name.names;												//genom att skapa en funktion.
- 	 // });
+
+
 	
-	 //  function names(persArr){						//en funktion som heter names.
-	 //      return names.sort(function (a, b){			//sorterar namnen i bokstavsordning
-	 //          return a.localeCompare(b);					//sorterar efter Å Ä och Ö.
-	 //      }).join(", ");}													//skriver ihop alla namn till en och samma sträng.
 
 
-
-return {minAge: minAge, names: namesStr};
+return {minAge: minAge, maxAge: maxAge, names: namesStr};
 
 };
-/*maxAge: maxAge,*/ /*: minAge,*/
+/*maxAge: maxAge,*/ /*minAge: minAge,*/
 
 // var data = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36}, {name: "Mats Loock", age: 46}];  //Arrayen som innehåller informationen.
 
