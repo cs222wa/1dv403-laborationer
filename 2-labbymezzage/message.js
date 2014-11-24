5011f0e86f04424779fd0c5df5914b7af5a23783
cs222wa <cs222wa@studen.lnu.se>
2014-11-24 15:47:59 +0100

    added working date and text functions.

--- 2-labbymezzage/message.js
+++ 2-labbymezzage/message.js
@@ -1,4 +1,31 @@
 "use strict";
-function Message(){  //Constructor function from where the objects will be instantiated.
+function Message(message, date) {  //Constructor function from where the objects will be instantiated.
 
+ this.getText = function(){
+  return message;
+ };
+
+this.setText = function(_text){
+ message = _text;
+};
+
+ this.getDate = function(){
+ return date;
+ };
+
+  this.setDate = function(_date){
+ date = _date;
+  }
 }
+
+Message.prototype.toString = function(){
+ return this.getText()+"("+this.getDate()+")";
+};
+
+Message.prototype.getHTMLText = function(){
+ return this.getText().replace(/ \n /g, "<br />");
+};
+
+Message.prototype.getDateText = function(){
+
+};
