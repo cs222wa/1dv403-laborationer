5004660daaeb4e023cb9fb1f87fb50cb6c4045ac
cs222wa <cs222wa@studen.lnu.se>
2014-11-25 13:18:55 +0100

    messages displays properly on page. Moving on to display message date and number of sent messages.

--- 2-labbymezzage/js/message.js
+++ 2-labbymezzage/js/message.js
@@ -23,7 +23,7 @@ Message.prototype.toString = function(){
 };
 
 Message.prototype.getHTMLText = function(){
- return this.getText().replace(/ \n /g, "<br />");
+ return this.getText().replace(/ \n /g, "<br/>");
 };
 
 Message.prototype.getDateText = function(){
--- 2-labbymezzage/js/messageboard.js
+++ 2-labbymezzage/js/messageboard.js
@@ -13,25 +13,29 @@ var MessageBoard  = {
 
     sentMsg: function() {
 
-        var msg = document.getElementById("textarea"); //collects the message written by user.
+        var msgArea = document.getElementById("textarea"); //tells object where to collect the message text.
+        console.log(msgArea.value);
+        MessageBoard.messages.push(new Message(msgArea.value, new Date())); //adds new object to array
+        var lastMsg = MessageBoard.messages.length -1; //select last message in the array,
+        MessageBoard.renderMessage(lastMsg); //sends selected message to be created on site
+    },
 
-
-        //add user message as an object to messages[] by useing the comman .push
-
-        //  Put anything into an array using Array.push().
-//        var a=[], b={};
-//          a.push(b);
-//          //a[0] === b;
-
-
-
-
-
-
+    renderMessage: function(messageID) {
+        var div = document.getElementById("display"); //select the div where you want your message displayed.
+        var msgText = document.createElement("p"); //create a p-tag to write out message
+        msgText.innerHTML = MessageBoard.messages[messageID].getHTMLText();   // get text from message
+        div.appendChild(msgText);     //add the text at the bottom of the selected div.
     }
-
 };
+window.onload = MessageBoard.init; //Placed last in script, otherwise it will become undefined.
 
-window.onload = MessageBoard.init; //Placed last in code, otherwise it will become undefined.
 
+/*
+To Do
+- Display time message was created (time only?)
+        Use msgDate.innerHTML = MessageBoard.messages[messageID].getDateText ?
+        append to div along with MsgText.
 
+- Create counter to display on the page.
+        Append/replace to div "sentmessages".
+*/
