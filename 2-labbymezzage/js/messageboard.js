f576d3b4b48ab6e8b61591299bde68c5a7e7a82d
cs222wa <cs222wa@studen.lnu.se>
2014-11-25 12:32:34 +0100

    Connected send-button to js-file.  Proceeding w. saving user message as object and add it to the message array.

--- 2-labbymezzage/js/messageboard.js
+++ 2-labbymezzage/js/messageboard.js
@@ -1,7 +1,37 @@
 "use strict";
 
-window.onload = function(){
 
-var msg = new Message("Testmeddelande", new Date());
+
+var MessageBoard  = {
+
+    messages: [],
+
+    init: function() {
+        var sent = document.getElementById("button");   //when user presses "send" button
+        sent.onclick = MessageBoard.sentMsg;            //the function sentMsg is called.
+    },
+
+    sentMsg: function() {
+
+        var msg = document.getElementById("textarea"); //collects the message written by user.
+
+
+        //add user message as an object to messages[] by useing the comman .push
+
+        //  Put anything into an array using Array.push().
+//        var a=[], b={};
+//          a.push(b);
+//          //a[0] === b;
+
+
+
+
+
+
+    }
 
 };
+
+window.onload = MessageBoard.init; //Placed last in code, otherwise it will become undefined.
+
+
