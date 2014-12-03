e2a3362a5cd947620de98582228337657149bb27
cs222wa <cs222wa@studen.lnu.se>
2014-12-03 13:23:26 +0100

    the set time for flipping tiles is working, but only the tile markes guess2 gets flipped, guess1 does not.

--- 3-gameon/memory/script/game.js
+++ 3-gameon/memory/script/game.js
@@ -10,6 +10,11 @@ var Memory = {
     renderBoard: function(gameArr){
 
     var board = document.getElementById("gameboard");
+        var guess1 ="";
+        var guess2 ="";
+        var totalTries = 0;
+        var tries = 0; //variable for number of attempts made
+        var finds = 0; //variable for amount of correct answers
 
 
         gameArr.forEach(function(tile, index){
@@ -24,16 +29,11 @@ var Memory = {
             picture.className= "bgimg";
             container.appendChild(picture);
 
-
             var link = document.createElement("a");
             link.setAttribute('href', '#');
-            link.onclick = function(e){
 
-                var guess1 ="";
-                var guess2 ="";
-                var totalTries = 0;
-                var tries = 0; //variable for number of attempts made
-                var finds = 0; //variable for amount of corrent answers
+
+            link.onclick = function(e){
 
                 if (tries < 2){
                     tries++;
@@ -49,7 +49,6 @@ var Memory = {
                         console.log("second try");
 
                         if(guess1 == guess2){  //if the source image for both guesses are the same it logs as a match.
-
                             finds++;
                             totalTries++;
                             console.log(finds);
@@ -57,18 +56,22 @@ var Memory = {
                         }
                         else{
                             totalTries++;
-                            setTimeout(function(){      //flips tiles after 1 second.
+                            setTimeout(function(){
+                                e.target.src ="pics/img0.jpg";
+                                e.target.alt = "Image of the Transformers logo.";
+                                tries = 0; //resets number of tries.
+                                  //flips tiles after 1 second.
                                 console.log("flipping tiles");
                             }, 1000);
-                            setTimeout(function(){      //clears console log after 6 secons.
-                                console.clear();
-                            }, 60000);
-                        }
 
+                        }
                     }
                     if(finds == 8){
-                        alert("Congratulations, you solved the game in" + tries + "tries!");
+                        alert("Congratulations, you solved the game in " + totalTries + " tries!");
                         console.log(tries);
+                        setTimeout(function(){      //clears console log after 1 secons.
+                            console.clear();
+                        }, 10000);
                     }
 
                     }
