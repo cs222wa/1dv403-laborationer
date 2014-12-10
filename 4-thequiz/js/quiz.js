095c6cdd0e936bcedb3db48eb255627e14a9a031
cs222wa <cs222wa@studen.lnu.se>
2014-12-10 14:54:11 +0100

    Question is displayed as text on web page.

--- 4-thequiz/js/quiz.js
+++ 4-thequiz/js/quiz.js
@@ -2,15 +2,23 @@
 window.onload = function(){
 
 
-
 //start game
 document.getElementById("startbutton").addEventListener("click", function(){
 
-var xhr = new XMLHttpRequest();
+    var xhr = new XMLHttpRequest();
 
 
-   xhr.onreadystatechange = function(){
-       console.log("svar");
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState == 4 && xhr.status === 200){
+            var question = JSON.parse(xhr.responseText);
+            console.log(question);
+            console.log(question.question);
+
+            var card = document.getElementById("questionarea");
+            var quiz = document.createElement("p");
+            quiz.innerHTML=question.question;
+            card.appendChild(quiz);
+        }
    };
 
     xhr.open("GET", "http://vhost3.lnu.se:20080/question/1", true);
@@ -19,20 +27,22 @@ var xhr = new XMLHttpRequest();
 
 
 
-
+/*
 
 //insert answer and send
     document.getElementById("startbutton").addEventListener("click", function() {
 
+        xhr.open()
     });
-
+*/
 
 };
 
 
-//Retrieve the question
-//turn it into usable code
-//present question on page
+//Retrieve the question         check
+//turn it into usable code      check
+//present question on page      check
+
 //send answer
 
 //You got your question! Now send me the answer via HTTP POST to the nextURL in JSON-format
