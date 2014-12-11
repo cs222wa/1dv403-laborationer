de863e2b9da5f22376fd94444a7d3d59c91becbf
cs222wa <cs222wa@studen.lnu.se>
2014-12-11 09:25:47 +0100

    Placed Quizgame inside an object Quiz

--- 4-thequiz/js/quiz.js
+++ 4-thequiz/js/quiz.js
@@ -1,7 +1,16 @@
 "use strict";
+/*
 window.onload = function(){
 
     //kapsla in i object för att kunna använda this/that
+
+
+    //Lägg allt nedanför i funktion "behandling av fråga"
+    // VId ny fråga - skicka in info som argument till funktionerna för
+    //att hämta korrekt fråga/svar.
+
+    //variabel för klick-momentet sparas för varje fråga
+    //variabel för klick-momentet (array) som sparar urlen man senast skickade till/fick?
 
 
     var url = "http://vhost3.lnu.se:20080/question/1";
@@ -12,13 +21,6 @@ document.getElementById("startbutton").addEventListener("click", function(){
 
     var xhr = new XMLHttpRequest();
 
-
-    //Lägg allt nedanför i funktion "behandling av fråga"
-    // VId ny fråga - skicka in info som argument till funktionerna för
-    //att hämta korrekt fråga/svar.
-
-    //variabel för klick-momentet sparas för varje fråga
-    //variabel för klick-momentet (array) som sparar urlen man senast skickade till/fick?
 
 
     xhr.onreadystatechange = function(){
@@ -70,22 +72,91 @@ document.getElementById("startbutton").addEventListener("click", function(){
 };
 
 
-//Retrieve the question         check
-//turn it into usable code      check
-//present question on page      check
+*/
 
-//send answer according to message:     check
-//You got your question! Now send me the answer via HTTP POST to the nextURL in JSON-format
-//
-//display if answer was right or wrong?
+
+
+var Quiz = {
+
+    init: function(){
+
+        var url = "http://vhost3.lnu.se:20080/question/1";
+        var click;
+
+        //start new game
+        document.getElementById("startbutton").addEventListener("click", function() {
+
+            var xhr = new XMLHttpRequest();
+             xhr.onreadystatechange = function () {
+                if (xhr.readyState == 4 && xhr.status === 200) {
+                    var question = JSON.parse(xhr.responseText);
+                    console.log(question);
+
+                    var card = document.getElementById("questionarea");
+                    var quiz = document.createElement("p");
+                    quiz.innerHTML = question.question;
+                    url = question.nextURL;
+                    card.appendChild(quiz);
+                } //if ends
+            }; //onreadystatechange ends
+
+
+            xhr.open("GET", url, true);
+            xhr.send(null);
+
+            //insert answer and send
+            document.getElementById("answerbutton").addEventListener("click", function() {
+
+                xhr.open("POST", url, true);
+                xhr.setRequestHeader("content-type", "application/json");
+
+                var response = document.getElementById("responsearea");
+                var jsonMessage = {
+                    answer: response.value
+                };
+                console.log(jsonMessage);
+
+                xhr.onreadystatechange = function(){
+
+
+                    if(xhr.readyState == 4 && xhr.status === 200){
+                        var question = JSON.parse(xhr.responseText);
+                        console.log(question);
+
+                        var card = document.getElementById("questionarea");
+                        var quiz = document.createElement("p");
+                        quiz.innerHTML=question.message;
+                        url=question.nextURL;
+                        card.appendChild(quiz);
+                    }
+                    if(xhr.readyState == 4 && xhr.status != 200){
+                        var question = JSON.parse(xhr.responseText);
+                        console.log(question);
+                        var card = document.getElementById("questionarea");
+                        var quiz = document.createElement("p");
+                        quiz.innerHTML=question.message;
+                        card.appendChild(quiz);
+                    }
+
+                };
+
+                xhr.send(JSON.stringify(jsonMessage));
+
+            });
+        });
+    }
+}; //object Quiz ends
+
+window.onload = Quiz.init;
 
 
 /*
- Svarar användaren rätt så tas man vidare till nästa fråga.
- Svarar man fel så får man reda på detta och får möjligheten att svara igen.
+ -/ Svarar användaren rätt så tas man vidare till nästa fråga.
+-/? Svarar man fel så får man reda på detta och får möjligheten att svara igen.
  I grundutförande ska applikationen hålla reda på hur många felaktiga svar man givit för varje fråga
  och när samtliga frågor har besvarats så ska resultatet presenteras
  där det framgår hur många försök man behövde för att klara varje fråga.
+
 
  Tänk på att skriva din applikation så generell så att den kan hantera förändringar i datan
  som ligger till grund för API:et. T.ex. så är det ju inte säkert att de frågenummer som
