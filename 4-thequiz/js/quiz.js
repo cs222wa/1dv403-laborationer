e78a8afa32462b473ecbfea57b8a5892a40b87fb
cs222wa <cs222wa@studen.lnu.se>
2014-12-11 18:04:34 +0100

    Fixed Issue with multiple error messages. Moving on to creating a counter for number of attempts made.

--- 4-thequiz/js/quiz.js
+++ 4-thequiz/js/quiz.js
@@ -1,104 +1,18 @@
 "use strict";
-/*
- window.onload = function(){
-
- //kapsla in i object för att kunna använda this/that
-
-
- //Lägg allt nedanför i funktion "behandling av fråga"
- // VId ny fråga - skicka in info som argument till funktionerna för
- //att hämta korrekt fråga/svar.
-
- //variabel för klick-momentet sparas för varje fråga
- //variabel för klick-momentet (array) som sparar urlen man senast skickade till/fick?
-
-
- var url = "http://vhost3.lnu.se:20080/question/1";
- var click;
-
- //start game
- document.getElementById("startbutton").addEventListener("click", function(){
-
- var xhr = new XMLHttpRequest();
-
-
-
- xhr.onreadystatechange = function(){
- if(xhr.readyState == 4 && xhr.status === 200){
- var question = JSON.parse(xhr.responseText);
- console.log(question);
-
- var card = document.getElementById("questionarea");
- var quiz = document.createElement("p");
- quiz.innerHTML=question.question;
- url=question.nextURL;
- card.appendChild(quiz);
- }
- };
-
- xhr.open("GET", url, true);
- xhr.send(null);
-
- //insert answer and send
- document.getElementById("answerbutton").addEventListener("click", function() {
-
- xhr.open("POST", url, true);
- xhr.setRequestHeader("content-type", "application/json");
-
- var response = document.getElementById("responsearea");
- var jsonMessage = {
- answer: response.value
- };
- console.log(jsonMessage);
-
- xhr.onreadystatechange = function(){
- if(xhr.readyState == 4 && xhr.status === 200){
- var question = JSON.parse(xhr.responseText);
- console.log(question);
-
- var card = document.getElementById("questionarea");
- var quiz = document.createElement("p");
- quiz.innerHTML=question.message;
- url=question.nextURL;
- card.appendChild(quiz);
- }
- };
-
- xhr.send(JSON.stringify(jsonMessage));
-
-
- });
- });
- };
-
-
- */
-
-
 var Quiz = {
     url: undefined,
-
+    tries: [],
     init: function(){              //start new game
         Quiz.url = "http://vhost3.lnu.se:20080/question/1"
         var click;
         var base = this;
-        /*document.getElementById("startbutton").addEventListener("click", function() {
-            var postQuestion = function(response){
-                var card = document.getElementById("questionarea");
-                var quiz = document.createElement("p");
-                quiz.innerHTML = response.question;
-                Quiz.url = response.nextURL;
-                card.appendChild(quiz);
-            };
-            Quiz.getQuestion(postQuestion);
-        });*/
-
         var startGame = function(){
             var postQuestion = function(response){
                 var card = document.getElementById("questionarea");
                 var quiz = document.createElement("p");
                 quiz.innerHTML = response.question;
                 Quiz.url = response.nextURL;
+                Quiz.tries.push(0); //set number of tries in array to 0
                 card.appendChild(quiz);
             };
             Quiz.getQuestion(postQuestion);
@@ -106,31 +20,37 @@ var Quiz = {
 
         document.getElementById("answerbutton").addEventListener("click", function() {
             var xhr = new XMLHttpRequest();
-
+            var card = document.getElementById("questionarea");
+            var toBeRemoved = document.getElementById("remove");
+            if(toBeRemoved != null){
+                toBeRemoved.parentNode.removeChild(toBeRemoved);
+            }
             var correct = function(response){
                 console.log(response);
-
-                var card = document.getElementById("questionarea");
-                var quiz = document.createElement("p");
-                quiz.innerHTML=response.message;
-                card.appendChild(quiz);
-
+                card.innerHTML="";
                 if(response.nextURL != undefined ){
                     Quiz.url=response.nextURL;
                     startGame();
                 }
                 else{
-                    //TODO Response if game over
+                    var gameOver = document.createElement("p");
+                    gameOver.innerHTML="Congratulations, you've answered all the questions!";
+                    var numberOfTries = document.createElement("p");
+                    numberOfTries.innerHTML="Question 1: " + Quiz.tries[0] + " tries.";
+                    card.appendChild(gameOver);
+                    card.appendChild(numberOfTries);
+                    //TODO Create P tag with number of tries per question.
+                    Quiz.tries.length = 0;// Empty number of tries
                     //TODO Create button for restart game = Empty Question area, call on init.
                 }
             };
 
             var wrong = function(response){
                 console.log(response);
-                var card = document.getElementById("questionarea");
-                var quiz = document.createElement("p");
-                quiz.innerHTML=response.message;
-                card.appendChild(quiz);
+                var wrongAnswer = document.createElement("p");
+                wrongAnswer.id="remove";
+                wrongAnswer.innerHTML = "Tyvärr, det var fel. Försök igen.";//response.message;
+                card.appendChild(wrongAnswer);
             };
         Quiz.sendAnswer(correct, wrong);
         });
@@ -139,7 +59,6 @@ var Quiz = {
 
 
     getQuestion: function(renderQuestion){
-
         var xhr = new XMLHttpRequest();
         xhr.onreadystatechange = function () {
             if (xhr.readyState == 4 && xhr.status === 200) {
@@ -152,7 +71,6 @@ var Quiz = {
     },
 
     sendAnswer: function(correct, wrong){
-
         var xhr = new XMLHttpRequest();
         xhr.open("POST", Quiz.url, true);
         xhr.setRequestHeader("content-type", "application/json");
@@ -163,7 +81,7 @@ var Quiz = {
         };
         console.log(jsonMessage);
         xhr.onreadystatechange = function(){
-
+            Quiz.tries.length[Quiz.tries.length-1]++;   //Add 1 to array every time an answer is given.
             if(xhr.readyState == 4 && xhr.status === 200){
                 var question = JSON.parse(xhr.responseText);
                 correct(question);
@@ -172,24 +90,21 @@ var Quiz = {
                 var question = JSON.parse(xhr.responseText);
                 wrong(question);
             }
-
         };
         xhr.send(JSON.stringify(jsonMessage));
     }
 }; //object Quiz ends
-
 window.onload = Quiz.init;
 
 
-//TODO Empty question-area with every new question
-//TODO Create click-counter for entire session.
-//TODO create click-counter for each game
+
+//TODO create click-counter for each question (array)
 //TODO Create CSS
 
 
 /*
  -/ Svarar användaren rätt så tas man vidare till nästa fråga.
- -/? Svarar man fel så får man reda på detta och får möjligheten att svara igen.
+ -/ Svarar man fel så får man reda på detta och får möjligheten att svara igen.
  I grundutförande ska applikationen hålla reda på hur många felaktiga svar man givit för varje fråga
  och när samtliga frågor har besvarats så ska resultatet presenteras
  där det framgår hur många försök man behövde för att klara varje fråga.
