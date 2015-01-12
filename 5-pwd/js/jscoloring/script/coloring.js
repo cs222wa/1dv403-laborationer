6129019b379e4d45f9926357fae17b29c15087a4
cs222wa <cs222wa@studen.lnu.se>
2015-01-12 19:17:17 +0100

    Basic drawing on canvas is working.

--- 5-pwd/js/jscoloring/script/coloring.js
+++ 5-pwd/js/jscoloring/script/coloring.js
@@ -18,6 +18,8 @@ PWD.Coloring.prototype.renderCanvas=function(){
     var base = this;
     base.canvas = document.createElement("canvas");
     base.canvas.className="canvas";
+    base.canvas.setAttribute('width', "780px");
+    base.canvas.setAttribute('height', "513px");
     base.contentDiv.appendChild(base.canvas);
     if(typeof G_vmlCanvasManager != 'undefined') {
         base.canvas = G_vmlCanvasManager.initElement(base.canvas);
@@ -25,38 +27,44 @@ PWD.Coloring.prototype.renderCanvas=function(){
      var context = base.canvas.getContext("2d");
 
 
-    var clickX = new Array();
-    var clickY = new Array();
-    var clickDrag = new Array();
     var paint = false;
 
     //When mousebutton is pressed down - the function finds the mouse's coordinates and calls the appropriate functions.
-    base.canvas.onmousedown(function(e){
+    base.canvas.onmousedown=function(e){
+        console.log("on mouse down");
         var mouseX = e.pageX - this.offsetLeft;
         var mouseY = e.pageY - this.offsetTop;
 
         paint = true;
         addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
         redraw();
-    });
+    };
 
     //When mousebutton is moved the function traces the mouse's coordinates and calls the appropriate functions..
-    base.canvas.onmousemove(function(e){
+    base.canvas.onmousemove=function(e){
+        console.log("on mouse move");
         if(paint){
             addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
             redraw();
         }
-    });
+    };
 
     //When the mousebutton is released OR mouse moves off the canvas area the function stops drawing.
-    base.canvas.onmouseup(function(e){
+    base.canvas.onmouseup=function(e){
+        console.log("on mouse up");
         paint = false;
-    });
-    base.canvas.onmouseout(function(e){
+    };
+    base.canvas.onmouseout=function(e){
+        console.log("on mouse out");
         paint = false;
-    });
+    };
 
     //this function saves the position of the mouse
+
+    var clickX = [];
+    var clickY = [];
+    var clickDrag = [];
+
     function addClick(x, y, dragging)
     {
         clickX.push(x);
@@ -67,7 +75,7 @@ PWD.Coloring.prototype.renderCanvas=function(){
     //this function saves and redraws all the information that has been "drawn" on the canvas
     //each time the function is called.
     function redraw(){
-        context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
+       context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
 
         context.strokeStyle = "#df4b26";
         context.lineJoin = "round";
